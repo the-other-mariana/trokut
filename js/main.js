67026aeebe4f0164c7005c92784d629eb13f157c
the-other-mariana <mariana.avalos.arce@gmail.com>
2022-07-25 13:12:51 -0500

    bufferGeom with vertices array works
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var scene = new THREE.Scene();
 // camera params: fov, aspect, near plane, far plane
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 // set camera position on the z axis
+// the higher in z, the further it goes
 camera.position.z = 5;
 // renderer (webGL) params: anti alias
 var renderer = new THREE.WebGLRenderer({antialias: true});
@@ -19,9 +20,31 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix();
 });
 
-var geometry = new THREE.SphereGeometry(1, 100, 100);
-var material = new THREE.MeshLambertMaterial({color: 0xFFCC00});
-var mesh = new THREE.Mesh(geometry, material);
+const geometry = new THREE.BufferGeometry();
+// create a simple square shape. We duplicate the top left and bottom right
+// vertices because each vertex needs to appear once per triangle.
+const vertices = new Float32Array( [
+	-1.0, -1.0,  1.0,
+	 1.0, -1.0,  1.0,
+	 1.0,  1.0,  1.0,
+
+	 1.0,  1.0,  1.0,
+	-1.0,  1.0,  1.0,
+	-1.0, -1.0,  1.0
+] );
+
+// itemSize = 3 because there are 3 values (components) per vertex
+geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+const mesh = new THREE.Mesh( geometry, material );
 
 scene.add(mesh);
+
+// in order to see colors, we need light
+// light params: color, intensity, distance
+var light = new THREE.PointLight(0xFFFFFF, 1, 500);
+// (x, y, z)
+light.position.set(10, 0, 25);
+scene.add(light);
+
 renderer.render(scene, camera);
