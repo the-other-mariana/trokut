132dde660663598038c2688c1e7429d2303a483a
the-other-mariana <mariana.avalos.arce@gmail.com>
2022-07-25 13:05:35 -0500

    built-in sphere shows in canvas
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- js/main.js
+++ js/main.js
@@ -7,19 +7,19 @@ camera.position.z = 5;
 var renderer = new THREE.WebGLRenderer({antialias: true});
 
 renderer.setClearColor("#e5e5e5"); // light gray
-renderer.setSize(window.innerWidth, window.innerWidth);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 // create a canvas with prev settings
 document.body.appendChild(renderer.domElement);
 
 // listen to window resize event
 window.addEventListener('resize', () => {
-    renderer.setSize(window.innerWidth, window.innerWidth);
+    renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 });
 
-var geometry = new THREE.SphereGeometry(1, 10, 10);
+var geometry = new THREE.SphereGeometry(1, 100, 100);
 var material = new THREE.MeshLambertMaterial({color: 0xFFCC00});
 var mesh = new THREE.Mesh(geometry, material);
 
