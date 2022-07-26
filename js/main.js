74e18af4680e8c086555efeaafb7cc991b4fe66d
the-other-mariana <mariana.avalos.arce@gmail.com>
2022-07-26 16:39:47 -0500

    depth slider sets zPos correctly
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ var g_transforms = [[]];
 
 var mode = "normal";
 
+function updateTextInput(val) {
+    zPos = parseFloat(val);
+    document.getElementById('textInput').value = val;
+}
+
 function getNewTransformMatrix(){
     var transformMatrix = new Float32Array([
       1.0, 0.0, 0.0, 0.0,
@@ -49,7 +54,7 @@ function mouseToScenePosition(ev, camera, scene){
     mouse.y = -(ev.clientY / window.innerHeight) * 2 + 1;
     // unit normal vector as normal of the plane
     planeNormal.copy(camera.position).normalize();
-    plane.setFromNormalAndCoplanarPoint(planeNormal, scene.position);
+    plane.setFromNormalAndCoplanarPoint(planeNormal, new THREE.Vector3(0, 0, zPos));
     // raycast from camera to mouse
     raycaster.setFromCamera(mouse, camera);
     // store objects in 2nd param
@@ -85,6 +90,15 @@ function renderScene(camera, renderer, scene){
 
         scene.add( verts );
     }
+
+    var origin = new Float32Array([0, 0, 0]);
+    const originGeo = new THREE.BufferGeometry();
+    originGeo.setAttribute( 'position', new THREE.Float32BufferAttribute( origin, 3 ) );
+    const originMat = new THREE.PointsMaterial( { color: 0x000000 } );
+    const originVerts = new THREE.Points( originGeo, originMat );
+
+    scene.add( originVerts );
+
     var light = new THREE.PointLight(0xFFFFFF, 1, 500);
     light.position.set(10, 0, 25);
     scene.add(light);
@@ -119,87 +133,6 @@ function main(){
 
     initTransforms();
     renderScene(camera, renderer);
-    /*
-    var scene = new THREE.Scene();
-    // camera params: fov, aspect, near plane, far plane
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    // set camera position on the z axis
-    // the higher in z, the further it goes
-    camera.position.z = 5;
-    // renderer (webGL) params: anti alias
-    var renderer = new THREE.WebGLRenderer({antialias: true});
-
-    renderer.setClearColor("#e5e5e5"); // light gray
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    // create a canvas with prev settings
-    var canvas = renderer.domElement;
-    canvas.setAttribute('id', 'canvas');
-    document.body.appendChild(canvas);
-    
-    canvas.onclick = function(ev){
-        console.log('canvas clicked!');
-        canvasClick(ev, canvas);
-    }
-
-
-    // listen to window resize event
-    window.addEventListener('resize', () => {
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-    });
-
-    const geometry = new THREE.BufferGeometry();
-    // create a simple square shape. We duplicate the top left and bottom right
-    // vertices because each vertex needs to appear once per triangle.
-    const vertices = new Float32Array( [
-        -1.0, -1.0,  1.0,
-        1.0, -1.0,  1.0,
-        1.0,  1.0,  1.0,
-
-        1.0,  1.0,  1.0,
-        -1.0,  1.0,  1.0,
-        -1.0, -1.0,  1.0
-    ] );
-
-    // itemSize = 3 because there are 3 values (components) per vertex
-    geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
-    const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-    const mesh = new THREE.Mesh( geometry, material );
-
-    scene.add(mesh);
-
-    const vertices2 = [];
-
-    for ( let i = 0; i < 10000; i ++ ) {
-
-        const x = randFloatSpread( 2000 );
-        const y = randFloatSpread( 2000 );
-        const z = randFloatSpread( 2000 );
-
-        vertices2.push( x, y, z );
-
-    }
-
-    const geometry2 = new THREE.BufferGeometry();
-    geometry2.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices2, 3 ) );
-
-    const material2 = new THREE.PointsMaterial( { color: 0x888888 } );
-
-    const points = new THREE.Points( geometry2, material2 );
-
-    scene.add( points );
-
-    // in order to see colors, we need light
-    // light params: color, intensity, distance
-    var light = new THREE.PointLight(0xFFFFFF, 1, 500);
-    // (x, y, z)
-    light.position.set(10, 0, 25);
-    scene.add(light);
-
-    renderer.render(scene, camera);
-    */
 }
 
 main();
